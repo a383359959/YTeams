f462fa42ddd410adbbf2bdda635974777c76264e
Qiu Hao <383359959@qq.com>
2018-05-10 21:52:41 +0800

    完善登录页面

--- main.js
+++ main.js
@@ -4,11 +4,37 @@ const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain;
 const dialog = electron.dialog;
 const net = electron.net;
+const session = electron.session;
 const path = require('path');
 const url = require('url');
+const ip = 'http://192.168.1.108/';
 let mainWindow;
 
-function createWindow () {
+function checkCookie(){
+    // 清除cookie
+    session.defaultSession.clearStorageData({
+        origin : ip,
+        storages : ['cookies']
+    },function(error){
+        if (error) console.error(error);
+    });
+    session.defaultSession.cookies.get({url : ip}, (error, cookies) => {
+        var member_id = '';
+        var token = '';
+        for(var i = 0;i < cookies.length;i++){
+            var row = cookies[i];
+            if(row.name == 'member_id') member_id = row.value;
+            if(row.name == 'token') token = row.value;
+        }
+        if(member_id != '' && token != ''){
+            createHome();
+        }else{
+            createLogin();
+        }
+    });
+}
+
+function createLogin(){
     mainWindow = new BrowserWindow({width : 320,height : 320,frame : false,resizable : false});
     mainWindow.loadURL(url.format({
         pathname : path.join(__dirname,'index.html'),
@@ -18,40 +44,39 @@ function createWindow () {
     mainWindow.on('closed',function(){
         mainWindow = null;
     });
+    mainWindow.webContents.openDevTools();
 };
 
-function log(string){
-    dialog.showMessageBox({
-        type : 'none',
-        title : '测试输出',
-        message : string
+function createHome(){
+    mainWindow = new BrowserWindow({frame : false,resizable : false});
+    mainWindow.loadURL(url.format({
+        pathname : path.join(__dirname,'home.html'),
+        protocol : 'file:',
+        slashes : true
+    }));
+    mainWindow.on('closed',function(){
+        mainWindow = null;
     });
 }
 
-function ajax(url,method){
-    const request = net.request({
-        method : method,
-        url : url
-    });
-    request.on('response',function(response){
-        response.on('data',function(result){
-            console.log(result);
-        });
-    });
-    request.end();
-}
-
-app.on('ready',createWindow);
+app.on('ready',checkCookie);
 
 app.on('window-all-closed',function(){
     if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', function(){
-    if (mainWindow === null) createWindow();
+    if (mainWindow === null) checkCookie();
 });
 
-ipc.on('login',function(event,args){
-    ajax('https://www.zhihu.com/api/v3/oauth/account/unlock/request','GET');
+ipc.on('dialog',function(event,args){
+    dialog.showMessageBox({
+        type : 'none',
+        title : args.title,
+        message : args.message
+    });
 });
 
+ipc.on('mainWindow',function(event,args){
+    createHome();
+});
